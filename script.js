02c7a8d1cf3b549f1ab7e65e20a252509780b9f2
xolodog <losididog@gmail.com>
2022-05-04 02:05:50 +0800

    Finish calculator functions

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 const display = document.getElementById('display');
+const wait = document.querySelector('#wait');
 const digits = document.querySelectorAll('.digits button');
+
+const addBtn = document.querySelector('#addBtn');
+const subtractBtn = document.querySelector('#subtractBtn');
+const multiplyBtn = document.querySelector('#multiplyBtn');
+const divideBtn = document.querySelector('#divideBtn');
+const equalBtn = document.querySelector('#equalBtn');
+const clearBtn = document.querySelector('#clearBtn');
+
+// Listeners
+clearBtn.addEventListener('click', () => {
+  display.textContent = '';
+});
+
+equalBtn.addEventListener('click', () => {
+  let curNum = +display.textContent;
+  let prevNum = +output[0];
+  let operator = output[1];
+  output = [];
+  let solution = operate(operator, prevNum, curNum);
+  display.textContent = solution;
+});
+
+digits.forEach(function (digit) {
+  digit.addEventListener('click', populateUI);
+});
+let output = [];
+const operateButtons = [addBtn, subtractBtn, multiplyBtn, divideBtn];
+operateButtons.forEach(function (button) {
+  button.addEventListener('click', () => {
+    let prevNum = display.textContent;
+    let operator = button.textContent;
+    display.textContent = '';
+    output.push(prevNum);
+    output.push(operator);
+    console.log(output);
+  });
+});
 
 // Functions
 function add(num1, num2) {
@@ -35,12 +73,6 @@ function operate(operator, num1, num2) {
   }
 }
 
-function populateDisplay() {
-  digits.forEach((digit) =>
-    digit.addEventListener('click', function () {
-      let chosenDigit = this.textContent;
-      display.textContent += chosenDigit;
-    })
-  );
+function populateUI() {
+  display.textContent += this.textContent;
 }
-populateDisplay();
