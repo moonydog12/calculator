e6cb37c85282df55460c1f54545c7d9be93e6093
xolodog <losididog@gmail.com>
2022-05-11 20:58:44 +0800

    Refractor

--- script.js
+++ script.js
@@ -9,38 +9,24 @@ const divideBtn = document.querySelector('#divideBtn');
 const equalBtn = document.querySelector('#equalBtn');
 const clearBtn = document.querySelector('#clearBtn');
 
-let displayNumber = '';
-let storedNumber = '';
-let operator = '';
+let displayNumber;
+let storedNumber;
+let operator;
 // Listeners
-clearBtn.addEventListener('click', () => {
-  displayZone.textContent = '';
-  storeZone.textContent = '';
-});
+clearBtn.addEventListener('click', clear);
 
-equalBtn.addEventListener('click', () => {
-  //將storedNumber、displayNumber轉成number形別並傳入operate fn
-  let solution = operate(operator, +storedNumber, +displayNumber);
-  displayNumber = solution;
-  storedNumber = '';
-  storeZone.textContent = storedNumber;
-  displayZone.textContent = displayNumber;
-});
+equalBtn.addEventListener('click', executeEqual);
 
 digits.forEach(function (digit) {
-  digit.addEventListener('click', populateUI);
+  digit.addEventListener('click', (e) => {
+    displayZone.textContent += e.target.textContent;
+    displayNumber = displayZone.textContent;
+  });
 });
 
 const operateButtons = [addBtn, subtractBtn, multiplyBtn, divideBtn];
 operateButtons.forEach(function (button) {
-  button.addEventListener('click', () => {
-    storedNumber = displayNumber;
-    displayNumber = '';
-    operator = button.textContent;
-    storeZone.textContent = `${storedNumber} ${operator}`;
-    displayZone.textContent = displayNumber;
-    return storedNumber, operator;
-  });
+  button.addEventListener('click', executeOperator);
 });
 
 // Functions
@@ -78,6 +64,37 @@ function operate(operator, num1, num2) {
 }
 
 function populateUI() {
-  displayZone.textContent += this.textContent;
-  displayNumber = displayZone.textContent;
+  displayZone.textContent = displayNumber;
+  storeZone.textContent = storedNumber;
+}
+
+function executeOperator(e) {
+  operator = e.target.textContent;
+  //檢查store區域是否有數字 沒有的話SET storeNumber to displayNumber THEN RESET displayNumber
+  if (!storeZone.textContent) {
+    console.log('store區域沒有資料');
+    storedNumber = displayNumber;
+    displayNumber = '';
+    populateUI();
+    return;
+  } else if (storeZone.textContent && displayZone.textContent) {
+    displayNumber = +displayZone.textContent;
+    storedNumber = +storeZone.textContent;
+    let solution = operate(operator, storedNumber, displayNumber);
+    storedNumber = '';
+    displayNumber = solution;
+    populateUI();
+    return;
+  }
+}
+
+function executeEqual() {
+  //將storedNumber、displayNumber轉成number形別並傳入operate fn
+}
+
+function clear() {
+  displayNumber = '';
+  storedNumber = '';
+  operator = '';
+  populateUI();
 }
