3d78c76f8327933134a676e9921c3a719df06f89
xolodog <losididog@gmail.com>
2022-06-14 17:14:04 +0800

    Modify calculator logic.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+/*元素選擇器*/
+
 const displayScreen = document.querySelector('#display');
 const digits = document.querySelectorAll('.buttons .digit-btn');
 const operators = document.querySelectorAll('.buttons .operator-btn');
@@ -5,16 +7,29 @@ const equalBtn = document.querySelector('#equalBtn');
 const clearBtn = document.querySelector('#clearBtn');
 const deleteBtn = document.querySelector('#deleteBtn');
 
+let clickedOperator = null;
+let storedNumber = null;
+/*行為監聽*/
+
 digits.forEach((digit) => {
   digit.addEventListener('click', () => {
     appendNumber(digit.innerHTML);
   });
 });
 
-clearBtn.addEventListener('click', clean);
+operators.forEach((operator) => {
+  operator.addEventListener('click', function (e) {
+    clickedOperator = operator.innerHTML;
+    storedNumber = displayScreen.innerHTML;
+    displayScreen.innerHTML = '';
+  });
+});
 
+clearBtn.addEventListener('click', clean);
 deleteBtn.addEventListener('click', deleteNumber);
-//點擊按鈕渲染數字到計算機螢幕
+equalBtn.addEventListener('click', clickEqualBtn);
+
+/*點擊按鈕渲染數字到計算機螢幕*/
 function appendNumber(number) {
   let value = displayScreen.textContent;
   if (value === '0' || value === 0) {
@@ -36,7 +51,29 @@ function deleteNumber() {
     displayScreen.innerHTML = '0';
   }
 }
-// 運算function
+
+function clickEqualBtn() {
+  let currentNumber = displayScreen.innerHTML;
+  //檢查是否接收到使用者輸入數字
+  if (storedNumber === null || storedNumber === undefined) {
+    alert('沒有輸入數字!');
+    return;
+  }
+  //檢查是否接收到使用者輸入運算符
+  if (
+    clickedOperator === null ||
+    clickedOperator === undefined ||
+    clickedOperator === ''
+  ) {
+    alert('沒有輸入運算符!');
+    return;
+  }
+  let answer = operate(storedNumber, clickedOperator, currentNumber);
+  storedNumber = null;
+  displayScreen.innerHTML = answer;
+}
+
+/* 運算function */
 function operate(a, operator, b) {
   // 把字串輸入轉成數字
   a = Number(a);
